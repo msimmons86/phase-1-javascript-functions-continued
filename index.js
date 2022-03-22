1d9fc323838c5218f801b1b0de5c57d08cdea362
Michael Simmons <msimmons86@gmail.com>
2022-03-21 18:35:05 -0700

    commit tree 6d3ca67a3b1f2fe55a06123bfeaa3dd239113fe3

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(workday = 'go to the office') {
+  return `This Monday, I will ${workday}.`;
+}
+
+function wrapAdjective(symbol = '*'){
+  return function(sentence='special') {
+  return `You are ${symbol}${sentence}${symbol}!`
+  }
+}
+
+
+
